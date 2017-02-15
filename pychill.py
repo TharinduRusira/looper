5470c24e11c5e5a8864dadae50bcf4e52eed99b8
Tharindu Rusira <tharindurusira@gmail.com>
2017-02-15 13:03:07 -0700

    py wrapper for CHiLL

---
+++ pychill.py
@@ -0,0 +1,7 @@
+'''
+A wrapper for CHiLL functionality that
+integrates loop-order handling to CHiLL
+
+Also, this will be used to help generate training data for
+transformation sequence prediction models
+'''
