5da725732fce403354288a3c14a5712676ef74e8
Tharindu Rusira <tharindurusira@gmail.com>
2017-02-18 16:57:19 -0700

    setup 'tile', extend for other xforms

--- sequencer.py
+++ sequencer.py
@@ -14,12 +14,15 @@ from opentuner import IntegerParameter
 from codegen import *
 from transform import *
 from scanner import *
+import time
+import os
+
 
 class Sequencer(MeasurementInterface):
 
     def __init__(self, xform, fn, p):
         self.xform = xform
-        self.cfile = {'name':fn, 'procedure': p}
+        self.cfile = {'path':fn, 'name': '{}'.format(fn.split('/')[-1]) , 'procedure': p}
         self.cg = CHiLLCodeGen()
 
     def config(self):
@@ -28,24 +31,45 @@ class Sequencer(MeasurementInterface):
         fdata = Scanner(self.cfile['name'])
 
         if self.xform is 'tile':
-            cm.add_parameter(EnumParameter('tile',['noop','tile8','tile16','tile32']))
+            cm.add_parameter(EnumParameter('size',[0,4,8,16,32]))
             cm.add_parameter(IntegerParameter('ll', 1, fdata['depth'])) #consider tiling for each loop
         elif self.xform is 'unroll':
-            cm.add_parameter(EnumParameter('unroll',['noop','unroll4','unroll8','unroll16']))
+            cm.add_parameter(EnumParameter('unroll',['unroll0','unroll4','unroll8','unroll16']))
         #add other xforms we allow
 
         else:
             print 'error: unknown transform \''+ self.xform +' \'\n'
 
+    def execute(self, config):
+        #consumes a configuration, generates and compile code, return SUCCESS/FAILURE
 
-    def run(self,desired_result,input,limit):
-        cfg = desired_result.configuration.data
         if self.xform is 'tile':
             #TODO: build a valid transformation and generate corresponding CHiLL script
-            cg.generate_chill_script(self.cfile['name'], self.cfile['procedure'], cfg['ll'], transformations=[] )
+            self.cg.generate_chill_script(self.cfile['name'], self.cfile['procedure'], looplevel1=0, transformations=['tile', 0, cfg['ll'], cfg['tile']] )
 
             #TODO: generate code, compile, run, measure time, repeat
 
+
+        RET = self.call_program('chill xform.script')
+        if(RET):
+            return -1e-10 #bad configuration
+        cmp_result = self.call_program('gcc -O3 rose_' + self.cfile['name'] +' -o bin.tmp')
+        assert cmp_result['returncode'] == 0
+
+        start = time.clock()*1000
+        run_result = self.call_program('./bin.tmp')
+        elapsed = time.clock()*1000 - start
+
+        os.remove('xform.chill')
+        os.remove('bin.tmp')
+
+
+        return elapsed
+
+    def run(self,desired_result,input,limit):
+        cfg = desired_result.configuration.data
+        ret_val = self.execute(cfg)
+        return Result(time = ret_val)
 
     def save_final_config(self, config):
         pass
