4b22f0b54ffe186bb49b41183954c2265aafa4cf
Tharindu Rusira <tharindurusira@gmail.com>
2017-03-01 11:37:48 -0700

    TODO: compute memory cost of the loop nest

--- costmodel.py
+++ costmodel.py
@@ -5,7 +5,8 @@ will consider arithmetic operations, memory accesses etc.
 
 def cost(mem, arith, tot_lines):
     #return cost
-    mc = mem/tot_lines
+    #mc = mem['memops']/tot_lines
+    mc = 1
     ac = (arith['add']+arith['sub']+8*arith['mul']+10*arith['div']+10*arith['mod'])/tot_lines    #weighted arithmetic cost per line
 
     return {'memcost':mc, 'arithcost': ac}
