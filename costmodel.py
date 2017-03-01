247c3c0e712ddb2c943ba9bf74fcb19dd92db9d3
Tharindu Rusira <tharindurusira@gmail.com>
2017-03-01 10:39:38 -0700

    weighted average for arithmetic cost

--- costmodel.py
+++ costmodel.py
@@ -6,6 +6,6 @@ will consider arithmetic operations, memory accesses etc.
 def cost(mem, arith, tot_lines):
     #return cost
     mc = mem/tot_lines
-    ac = arith/tot_lines    #TODO: weight arithmetic operations and compute weighted average
+    ac = (arith['add']+arith['sub']+8*arith['mul']+10*arith['div']+10*arith['mod'])/tot_lines    #weighted arithmetic cost per line
 
     return {'memcost':mc, 'arithcost': ac}
