ab11b0b9c3ac6d1907f0d15164c9e769b25bfc46
Tharindu Rusira <tharindurusira@gmail.com>
2017-04-02 18:37:22 -0600

    minor changes

--- costmodel.py
+++ costmodel.py
@@ -3,10 +3,15 @@ defines a simple cost model to evaluate the nature of loop statements
 will consider arithmetic operations, memory accesses etc.
 '''
 
-def cost(mem, arith, tot_lines, tot_itrs):
+def cost(mem, arith, tot_lines, tot_itrs, nests, num_nests):
     #return cost
-    #mc = mem['memops']/tot_lines
-    mc = float(mem['ref']*tot_itrs/tot_lines)
-    ac = float(arith['add']+arith['sub']+8*arith['mul']+10*arith['div']+10*arith['mod'])/float(tot_lines)    #weighted arithmetic cost per line
+
+    mc = []         #mem cost per nest
+    ac = []         #arith cost per nest
+
+    #modify tot_itrs with nest info
+    for n in range(num_nests):
+        mc.append(float(mem[n]['ref']*tot_itrs[n]/tot_lines))
+        ac.append(float(arith[n]['add']+arith[n]['sub']+8*arith[n]['mul']+10*arith[n]['div']+10*arith[n]['mod'])/float(tot_lines))    #weighted arithmetic cost per line
 
     return {'memcost':mc, 'arithcost': ac}
